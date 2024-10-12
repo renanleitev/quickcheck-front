22ef61f2b9c0ec66d5a2a6c3f216d88543117ca1
Renan Leite Vieira <renanleitev@gmail.com>
2024-10-12 20:06:23 -0300

    chore: Fix README

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
-# Quickchek
+# QuickChek
 
 Aplicativo para marcação de consultas.
+
+## Instalação
+
+Instale as dependências
+
+    npm i
+
+Para rodar a aplicação
+
+    npm start
+
+Acesse o endereço `localhost:9090` no seu navegador para visualizar a aplicação
 
 ## Equipe
 
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const port = 9090;
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
+  preview: {
+    port: port
+  },
+  server: {
+    port: port
+  }
 })
